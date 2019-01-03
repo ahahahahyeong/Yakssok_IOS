35297384b153da7fc31fae36cc787fb2d17150e9
song <eluad123@naver.com>
2019-01-03 16:33:44 +0900

    Initial Commit

---
+++ Yakssok/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yakssok
+//
+//  Created by 402-30 on 03/01/2019.
+//  Copyright © 2019 402-30. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
